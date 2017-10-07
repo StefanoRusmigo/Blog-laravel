1d1440da539e1e9bce180814ac032ca899d56809
Stefano Rusmigo <stefanorusmigo@hotmail.com>
2017-10-07 18:16:24 +0300

    testing

--- app/Post.php
+++ app/Post.php
@@ -21,16 +21,16 @@ class Post extends Model
 
     public function scopeFilter($query,$filters)
     {
-    	if ($month = $filters['month'])
+    	if (isset($filters['month']))
         {
-            $query->whereMonth('created_at',Carbon::parse($month)->month);
+            $query->whereMonth('created_at',Carbon::parse($filters['month'])->month);
 
         }
 
 
-        if ($year = $filters['year'])
+        if ($year = isset($filters['year']))
         {
-            $query->whereYear('created_at',$year);
+            $query->whereYear('created_at',$filters['year']);
 
         }
     }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Unit;
 
+use App\Post;
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+	use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -15,5 +18,29 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+    }
+
+    public function testArchives()
+    {
+    	$first = factory(Post::class)->create();
+    	$second = factory(Post::class)->create([
+    		'created_at'=>\Carbon\Carbon::now()->subMonth()]);
+
+    	$posts = Post::archives();
+
+    	$this->assertEquals([
+    		[
+    		  "year" => $first->created_at->format('Y'),
+    		  "month" => $first->created_at->format('F'),
+   			  "puplished" => 1
+  			],
+
+    		[
+    		  "year" => $second->created_at->format('Y'),
+    		  "month" => $second->created_at->format('F'),
+   			  "puplished" => 1
+  			]
+    		],$posts);
+    	
     }
 }
